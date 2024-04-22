4b84b59a3437f6e888634b05c535b1adee38bc1b
shafqat007 <shafqatnawazchy@gmail.com>
2024-04-22 21:09:17 +0600

    first

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import FetchData from './src/index';
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="FetchData"
+        screenOptions={{
+          headerShown: false,
+          animation: 'slide_from_right', 
+        }}
+      >
+        <Stack.Screen name="FetchData" component={FetchData} />
+        {/* <Stack.Screen name="Credit" component={Credit} /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ config.js
@@ -0,0 +1,24 @@
+//always same
+import firebase from 'firebase/compat/app';
+import {getDatabase}from 'firebase/database';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBuM1rstTbrMyoAFpm6C_MWVbRbib76UsQ",
+    authDomain: "robodoc-f8b8c.firebaseapp.com",
+    databaseURL: "https://robodoc-f8b8c-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "robodoc-f8b8c",
+    storageBucket: "robodoc-f8b8c.appspot.com",
+    messagingSenderId: "579335688578",
+    appId: "1:579335688578:web:c946251aa51bb3606bff9b",
+    measurementId: "G-E2WBB8TH1M"
+  };
+
+
+
+
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+const db = getDatabase();
+export { db }
